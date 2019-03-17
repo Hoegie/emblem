f0c90858872c9414b907e25356ba6a03790fc4e8
Hoegie <mezenhof8@gmail.com>
2019-03-17 12:00:55 +0100

    first commit

---
+++ footcalhook.sh
@@ -0,0 +1,3 @@
+date >> /app/nodeprojects/github/logs/footcalhook_5005.log 2>&1
+cd /app/nodeprojects/github/emblem
+git pull https://github.com/Hoegie/emblem.git >> /app/nodeprojects/github/logs/footcalhook_5005.log 2>&1
---
+++ footcalini.js
@@ -0,0 +1,9 @@
+module.exports.clubID = '5';
+module.exports.clubName = 'KRC EMBLEM';
+module.exports.clubBaseNr = '5180';
+module.exports.apisec = false;
+module.exports.dbname = 'FootCal_Emblem';
+module.exports.apachedir = 'emblem';
+module.exports.apiport = 4005;
+module.exports.serveraddress = "185.224.89.139";
+
